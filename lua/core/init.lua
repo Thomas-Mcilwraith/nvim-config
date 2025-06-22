90ed34b3f84757a347599670fa8fb6b1a208faaa
Thomas-Mcilwraith <thomas_mcilwraith@outlook.com>
2025-06-22 19:22:41 +0100

    fix: pyright not detecting other files in cwd()

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,3 +2,22 @@ require("utils")
 require("core.keymaps")
 require("core.set")
 require("core.lazy")
+
+-- clangd setup
+require('lspconfig').clangd.setup {
+    cmd = { "clangd",
+        "--compile-commands-dir=breadboard-drone-flight-code" ,
+        "--query-driver=**/arm-none-eabi-*"},
+    root_dir = require('lspconfig.util').root_pattern('compile_commands.json', '.git'),
+}
+
+-- pyright corrected setup (resolves import issues in python)
+require('lspconfig').pyright.setup({
+  settings = {
+    python = {
+      analysis = {
+        extraPaths = { "." },
+      },
+    },
+  },
+})
