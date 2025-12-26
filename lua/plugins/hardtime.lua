73ec1b70f4b60d7754ced688faa49c18f5c4443d
Thomas-Mcilwraith <thomas_mcilwraith@outlook.com>
2025-12-26 08:49:43 +0000

    chore: clean existing config

--- lua/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    opts = { disabled_keys = {
-        ['<Right>'] = { 'n', 'v' },
-        ['<Left>'] = { 'n' , 'v' },
-    } },
-}
--- lua/utils.lua
+++
@@ -1,9 +0,0 @@
-local utils = {}
-
-function utils.lsp_map(keys,func,desc,mode)
-	mode = mode or 'n'
-	local event = 'VimEnter'
-	vim.keymap.set(mode,keys,func, {buffer = event.buf, desc = 'LSP: ' .. desc })
-end
-
-return utils
